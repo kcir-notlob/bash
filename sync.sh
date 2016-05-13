580d9d63d918b90b949bef71d71d4006877a6b51
rick <rick@blacknest.gov.uk>
2016-05-13 16:59:51 +0100

    added a silly comment

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 #sync.sh
+#silly comment
 #rick a simple script to keep the ubuntu iso images upto date
 #zsync -i ubuntu-15.10-desktop-amd64.iso http://cdimage.ubuntu.com/ubuntu-gnome/releases/15.10/release/ubuntu-gnome-15.10-desktop-amd64.iso.zsync
 
