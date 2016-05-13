6b87d7ebea424eda22bc5402f396de57196ee1a9
kcir-notlob <kcir.notlob@gmail.com>
2016-05-13 16:39:42 +0100

    Add files via upload

---
+++ sync.sh
@@ -0,0 +1,6 @@
+#sync.sh
+#rick a simple script to keep the ubuntu iso images upto date
+#zsync -i ubuntu-15.10-desktop-amd64.iso http://cdimage.ubuntu.com/ubuntu-gnome/releases/15.10/release/ubuntu-gnome-15.10-desktop-amd64.iso.zsync
+
+
+zsync -i  xenial-desktop-amd64.iso  http://cdimage.ubuntu.com/daily-live/current/xenial-desktop-amd64.iso.zsync
