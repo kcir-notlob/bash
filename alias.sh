00952a54cba565ae5842f1c03b73674f3f3487b9
test <test@localhost.localdomain>
2018-05-30 15:57:34 +0100

    add vim remote

--- alias.sh
+++ alias.sh
@@ -1,3 +1,4 @@
 #command line Kung Fu
 alias top_ten="history|awk '{print $2}'|sort|uniq -c|sort -rn|head"
 alias clean="grep -Ev '^#|^$'"
+alias vim_remote="vim scp://name@server//home/user/vim_test/test.txt"
